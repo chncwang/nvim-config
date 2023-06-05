dfb46e46e0608d28bbc2bdb259752135dd66be22
Wang Qiansheng <chncwang@Wangs-MacBook-Air.local>
2023-06-05 08:50:55 +0800

    Remove local Neovim config file copy script

--- update_from_local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copy the local Neovim config file to the project directory
-cp ~/.config/nvim/init.lua ./init.lua
-
-# Print a message indicating that the copy was successful
-echo "Copied local Neovim Lua config file to project directory"
