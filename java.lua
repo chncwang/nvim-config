830daa23c995e0ec105210deb4f6b86e548943f6
Wang Qiansheng <chncwang@Wangs-MacBook-Air.local>
2023-06-05 08:48:34 +0800

    Add Java LSP key mappings and formatting

--- java.lua
+++ java.lua
@@ -3,3 +3,7 @@ local config = {
     root_dir = vim.fs.dirname(vim.fs.find({'.git', 'pom.xml'}, {upward = true})[1]),
 }
 require('jdtls').start_or_attach(config)
+
+vim.api.nvim_set_keymap('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', {noremap = true, silent = true})
+vim.api.nvim_command('autocmd BufWritePre *.java lua vim.lsp.buf.format({async = false})')
+vim.api.nvim_command('autocmd BufWritePost *.java lua require"jdtls".organize_imports()')
