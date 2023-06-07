8366954ac5d4c4ea9da06f73b015ee86bab70566
Chauncey Wang <chncwang@gmail.com>
2023-06-08 07:16:31 +0800

    Add telescope to init.lua

--- init.lua
+++ init.lua
@@ -56,6 +56,12 @@ require('packer').startup(function()
 
   -- file explorer
   use 'preservim/nerdtree'
+  use 'nvim-tree/nvim-web-devicons'
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { {'nvim-lua/plenary.nvim'}, {'BurntSushi/ripgrep'} }
+  }
 
   -- quick edit
   use 'hrp/EnhancedCommentify'
@@ -124,3 +130,10 @@ vim.g.ale_echo_msg_error_str = '%code: %%s'
 vim.o.textwidth = 120
 vim.o.wrap = true
 map('n', '<leader>a', ':ALEDetail<CR>', options)
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
